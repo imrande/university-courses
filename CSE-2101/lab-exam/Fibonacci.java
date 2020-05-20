8d286f0e3393e99dea8f71d515016e56abb0af86
Imran Ahmed <58659161+imrande@users.noreply.github.com>
2020-05-20 08:42:47 +0600

    lab-exam

---
+++ CSE-2101/lab-exam/Fibonacci.java
@@ -0,0 +1,14 @@
+// prgoram for nth fibonacci number
+
+class Fibonacci { 
+    static int fib(int n) { 
+		if (n <= 1) 
+		   return n; 
+		return fib(n-1) + fib(n-2); 
+    } 
+       
+    public static void main (String args[]) { 
+		int n = 9; 
+		System.out.println(fib(n)); 
+    } 
+} 
